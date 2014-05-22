f98b6bc0c04b20c3a4e963dccae20ec415cbc8d5
youngytj <youngytj@sina.com>
2014-05-22 12:55:42 +0800

    --  New  上传PC端程序

---
+++ PC端程序/STM32Control/STM32Core/Buffer/BufferManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace STM32Core
+{
+    internal sealed class BufferManager
+    {
+        private byte[] buffer;
+        private int bufferSize;
+        private int numSize;
+        private int currentIndex;
+        private Stack<int> freeIndexPool;
+
+        internal BufferManager(int numsize, int buffersize)
+        {
+            this.numSize = numsize;
+            this.bufferSize = buffersize;
+        }
+
+        internal void Inint()
+        {
+            buffer = new byte[numSize];
+            freeIndexPool = new Stack<int>(numSize / bufferSize);
+        }
+
+        internal void FreeBuffer(SocketAsyncEventArgs args)
+        {
+            freeIndexPool.Push(args.Offset);
+            args.SetBuffer(null, 0, 0);
+        }
+
+        internal Boolean SetBuffer(SocketAsyncEventArgs args)
+        {
+            if (this.freeIndexPool.Count > 0)
+            {
+                args.SetBuffer(this.buffer, this.freeIndexPool.Pop(), this.bufferSize);
+            }
+            else
+            {
+                if ((this.numSize-this.bufferSize)<this.currentIndex)
+                {
+                    return false;
+                }
+                args.SetBuffer(this.buffer, this.currentIndex, this.bufferSize);
+                this.currentIndex += this.bufferSize;
+            }
+            return true;
+        }
+    }
+}
---
+++ PC端程序/STM32Control/STM32Core/Proxy/Processor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace STM32Core
+{
+    /// <summary>
+    /// 连接通知
+    /// </summary>
+    /// <param name="message"></param>
+    public delegate void OnConnect(bool message);
+    /// <summary>
+    /// 消息通知
+    /// </summary>
+    /// <param name="data"></param>
+    public delegate void OnData(byte[] data);
+    /// <summary>
+    /// 断开连接通知
+    /// </summary>
+    /// <param name="message"></param>
+    public delegate void OnDisconnect(string message);
+}
