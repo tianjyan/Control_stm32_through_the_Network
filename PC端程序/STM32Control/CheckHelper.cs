d17f8810779f8ae2783d592caaad6b3173d93cc5
youngytj <youngytj@sina.com>
2014-05-22 12:33:01 +0800

    ----  New  上传PC端程序

---
+++ PC端程序/STM32Control/CheckHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace STM32Control
+{
+    public static class CheckHelper
+    {
+        /// <summary>
+        /// 验证IP地址
+        /// </summary>
+        /// <param name="IP">IP的字符串</param>
+        /// <returns></returns>
+        public static bool IPCheck(string IP)
+        {
+            string num = "(25[0-5]|2[0-4]\\d|[0-1]\\d{2}|[1-9]?\\d)";
+            return Regex.IsMatch(IP, ("^" + num + "\\." + num + "\\." + num + "\\." + num + "$"));
+        }
+
+        /// <summary>
+        /// 验证输入角度是否正确,180度
+        /// </summary>
+        /// <param name="Degree"></param>
+        /// <returns></returns>
+        public static bool DegreeCheck(string Degree)
+        {
+            //360度的正则表达式^\d$|^[1-9]\d$|^[1-2]\d\d$|^3[0-5]\d|^360$
+            return Regex.IsMatch(Degree, "^\\d$|^[1-9]\\d$|^1[0-7]\\d$|^180$");
+        }
+
+        /// <summary>
+        /// 验证端口号(0-1400)
+        /// </summary>
+        /// <param name="Port"></param>
+        /// <returns></returns>
+        public static bool PortCheck(string Port)
+        {
+            return Regex.IsMatch(Port, "^\\d$|^[1-9]\\d$|^[1-9]\\d{2}$|^1[1-3]\\d{2}$|^1400$");
+        }
+    }
+}
---
+++ PC端程序/STM32Control/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace STM32Control
+{
+    public class Startup
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            SingleApp sa = new SingleApp();
+            //string[] s = { "STM32Control" };
+            sa.Run(args);
+        }
+    }
+
+    public class SingleApp : WindowsFormsApplicationBase
+    {
+        public SingleApp()
+        {
+            this.IsSingleInstance = true;
+        }
+
+        private STM32Control.App app;
+        protected override bool OnStartup(Microsoft.VisualBasic.ApplicationServices.StartupEventArgs eventArgs)
+        {
+            app = new App();
+            //app.InitializeComponent();
+            app.Run();
+            return false;
+        }
+
+        protected override void OnStartupNextInstance(StartupNextInstanceEventArgs eventArgs)
+        {
+            //app.TryActive();
+            foreach (Window win in app.Windows)
+            {
+                if (win.Visibility != Visibility.Visible)
+                {
+                    win.Activate();
+                }
+            }
+        }
+
+    }
+}
